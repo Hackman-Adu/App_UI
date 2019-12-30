fd3b68bc044fec70100e2ce5f2ff67338d609d04
hackman <adugyamfihackman31@gmail.com>
2019-12-30 11:10:17 +0000

    Add project files.

---
+++ LoginScreen/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+
+namespace LoginScreen
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            SupportActionBar.Title = "iNN Diary";
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_items, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int i = item.ItemId;
+            if(i==Resource.Id.close)
+            {
+                Android.App.AlertDialog.Builder closing = new Android.App.AlertDialog.Builder(this);
+                closing.SetTitle("Closing Application");
+                closing.SetMessage("Do you want to close iNN Diary now?");
+                closing.SetPositiveButton("Yes", delegate
+                {
+                    this.Finish();
+                });
+                closing.SetNegativeButton("No", delegate
+                {
+                    //do nothing here
+
+                });
+
+                Android.App.AlertDialog alert = closing.Create();
+                alert.Show();
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
